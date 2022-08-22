c69c4f230c0f1ebe3a3e5b73ffd64e3749712a1d
Adam Knight <adamknight1992@gmail.com>
2022-08-22 10:25:08 +0100

    image loop change for testing

--- imaging.f90
+++ imaging.f90
@@ -167,7 +167,7 @@ module imaging
 
             print "(a)", adjustr(trim("Writing image files"))
 
-            do k = 2, 2     
+            do k = 1, 2     
                 do t = 1, NumberOfTimePoints
                     write(imageNumber, '(I0.3)') ((t*tstep_int)-(1*tstep_int)+start_int)
                     if (k == 1) then                
